73e67ec8e07889a26986e8083d8405b8ea507fbb
LucaSalvadori <salva.luca003@gmail.com>
2022-10-06 12:43:15 +0200

    First global and display lib

---
+++ Sauna/Sauna.ino
@@ -0,0 +1,40 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+float tmp_int;
+float tmp_off = 70;
+
+enum Pow_level {OFF, ONE, TWO, TREE};
+Pow_level power = OFF;
+Pow_level pow_max = TREE;
+
+enum Programm {STANDBY, ON, ON_LOW_POW, ERROR_PROGRAMM};
+Programm programm = STANDBY;
+
+enum Page {INFO, SETTING, ERROR_PAGE};
+
+Page page = INFO;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
