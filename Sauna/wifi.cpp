07f4c84b396f6f6b549b4e3605efa1b2869a6c1d
LucaSalvadori <salva.luca003@gmail.com>
2022-10-17 23:05:54 +0200

    wifi

---
+++ Sauna/wifi.cpp
@@ -0,0 +1,20 @@
+
+bool initWifi(){
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    
+    server.begin();
+
+}
